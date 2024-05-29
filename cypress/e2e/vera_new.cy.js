dc4db688de0fb937310394e0865524576279d132
Vera <117353873+yunvera@users.noreply.github.com>
2024-05-29 23:47:19 +0300

    Add files via upload

---
+++ cypress/e2e/vera_new.cy.js
@@ -0,0 +1,85 @@
+describe('Тестирование формы авторизации', function () {
+
+    it('Верный логин и пароль', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('Проверка восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio111');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german123@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Проверка валидации логина', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Проверка логина на приведение к строчным буквам', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+ })
+
+ describe('Покемоны', function () {
+
+    it('Тестирование покупкм аватара', function () {
+         cy.visit('https://pokemonbattle.me');
+         cy.get(':nth-child(1) > .auth__input').type('USER_LOGIN'); // заменить USER_LOGIN - на логин
+         cy.get('#password').type('USER_PASSWORD'); // заменить USER_PASSWORD - на пароль
+         cy.get('.auth__button').click();
+         cy.get('.header__btns > [href="/shop"]').click();
+         cy.get('.available > button').first().click();
+         cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4111111111111111');
+         cy.get(':nth-child(1) > .pay_base-input-v2').type('07/30')
+         cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type(125);
+         cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('NAME');
+         cy.get('.pay-btn').click();
+         cy.get('#cardnumber').type('56456');
+         cy.get('.payment__submit-button').click();
+         cy.get('.payment__font-for-success').contains('Покупка прошла успешно');
+         cy.get('.payment__font-for-success').should('be.visible');
+         cy.get('.success__image').should('be.visible');
+     })
+ })
